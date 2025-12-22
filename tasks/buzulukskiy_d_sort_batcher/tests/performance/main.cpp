ee8884c5e207eb49b97a176fc3252fdb0152439b
Danila Buzuluckski <danila.buz@mail.ru>
2025-12-23 00:32:16 +0300

    fix9

--- tasks/buzulukskiy_d_sort_batcher/tests/performance/main.cpp
+++ tasks/buzulukskiy_d_sort_batcher/tests/performance/main.cpp
@@ -31,7 +31,7 @@ class BuzulukskiyDSortBatcherPerfTests : public ppc::util::BaseRunPerfTests<InTy
   }
 
  private:
-  static constexpr std::size_t kCount = 100000;
+  static constexpr std::size_t kCount = 500000;
   InType input_data_;
 };
 
