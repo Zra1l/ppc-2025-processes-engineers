fb8e1e3c7b5f73ed89d649e98b9bb9c295298a4a
Danila Buzuluckski <danila.buz@mail.ru>
2025-12-22 22:40:37 +0300

    fix2

--- tasks/buzulukskiy_d_sort_batcher/tests/functional/main.cpp
+++ tasks/buzulukskiy_d_sort_batcher/tests/functional/main.cpp
@@ -37,16 +37,16 @@ class BuzulukskiyDSortBatcherFuncTests : public ppc::util::BaseRunFuncTests<InTy
 namespace {
 
 const std::array<TestType, 10> kTestParams = {
-    TestType{{}, "empty"},
-    TestType{{5}, "single_element"},
-    TestType{{3, 5, 21, 1, 4}, "random_5"},
-    TestType{{1, 2, 3, 4, 5}, "already_sorted"},
-    TestType{{-1, -5, 3, 0, 2}, "mixed_negative"},
-    TestType{{1, 1, 1, 1}, "all_equal"},
-    TestType{{1000, -1000, 500, -500, 250, -250}, "wide_range"},
-    TestType{{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}, "random_10"},
-    TestType{{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, "reverse_sorted"},
-    TestType{{4, -2, 7, 0, -2, 9, 1}, "uneven_distribution"},
+    TestType{InType{}, "empty"},
+    TestType{InType{5}, "single_element"},
+    TestType{InType{3, 5, 21, 1, 4}, "random_5"},
+    TestType{InType{1, 2, 3, 4, 5}, "already_sorted"},
+    TestType{InType{-1, -5, 3, 0, 2}, "mixed_negative"},
+    TestType{InType{1, 1, 1, 1}, "all_equal"},
+    TestType{InType{1000, -1000, 500, -500, 250, -250}, "wide_range"},
+    TestType{InType{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}, "random_10"},
+    TestType{InType{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, "reverse_sorted"},
+    TestType{InType{4, -2, 7, 0, -2, 9, 1}, "uneven_distribution"},
 };
 
 const auto kTasks = std::tuple_cat(
